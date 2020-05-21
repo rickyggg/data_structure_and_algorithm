2da6fb1955f304d5df13a5fe2b93f7db9ef0727d
Jiarui Guo <guojiarui@smail.nju.edu.cn>
2020-05-21 09:22:14 +0800

    Update and rename Bubble to Bubble.cpp

---
+++ Algorithm/Sort/Bubble.cpp
@@ -0,0 +1,13 @@
+// O(N^2)
+void BubbleSort(int list[], int len) {
+    for (int i = 0; i < len; i++) {
+        int flag = 0;
+        for (int j = 1; j < len - i; j++) {
+            if (list[j - 1] > list[j]) {
+                swap(list[j - 1], list[j]);
+                flag = 1;
+            }
+        }
+        if (!flag) break;
+    }
+}
