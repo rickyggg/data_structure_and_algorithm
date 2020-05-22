336df81188af15a88368aaf3b28826d8b49cc633
Jiarui Guo <guojiarui@smail.nju.edu.cn>
2020-05-22 11:05:24 +0800

    Create Insert.cpp

---
+++ Sort/Insert.cpp
@@ -0,0 +1,11 @@
+// O(N)
+void InsertSort(int list[], int len) {
+    int j;
+    int temp;
+    for (int i = 1; i < len; i++) {
+        temp = list[i];
+        for (j = i - 1; j >= 0 && list[j] > temp; j--)
+            list[j + 1] = list[j];
+        list[j + 1] = temp;
+    }
+}
