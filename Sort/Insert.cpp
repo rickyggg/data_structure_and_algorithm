ac83869aa9db73c3090ead9d393da006a82a197d
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-05-22 12:29:29 +0800

    first commit

--- Sort/Insert.cpp
+++ Sort/Insert.cpp
@@ -1,11 +1,11 @@
 // O(N^2)
 void InsertSort(int *list, int len) {
-    int j;
-    int temp;
-    for (int i = 1; i < len; i++) {
-        temp = list[i];
-        for (j = i - 1; j >= 0 && list[j] > temp; j--)
-            list[i] = list[j];
-        list[j + 1] = temp;
-    }
+  int j;
+  int temp;
+  for (int i = 1; i < len; i++) {
+    temp = list[i];
+    for (j = i - 1; j >= 0 && list[j] > temp; j--)
+      list[i] = list[j];
+    list[i] = temp;
+  }
 }
