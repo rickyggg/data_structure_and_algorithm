6e5a7b6c87f6c0485e3c60dfb5a189d26d3c9918
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-05-24 10:41:50 +0800

    First Commit

--- Sort/Shell.cpp
+++ Sort/Shell.cpp
@@ -1,12 +1,14 @@
+// O(N^2)
 void shell(int *list, int n) {
   int j, d;
+  int tmp;
   for (d = n / 2; d > 0; d /= 2) {
-    for (int i = d + 1; i <= n; i++) {
+    for (int i = d; i < n; i++) {
       if (list[i] < list[i - d]) {
-        list[0] = list[i];
-        for (j = i - d; j > 0 && list[0] < list[j]; j -= d)
+        tmp = list[i];
+        for (j = i - d; j >= 0 && tmp < list[j]; j -= d)
           list[j + d] = list[j];
-        list[j + d] = list[0];
+        list[j + d] = tmp;
       }
     }
   }
