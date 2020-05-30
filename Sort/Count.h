91fa2a6066a5161be61f0730bd79583e0008d1c5
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-05-30 10:03:48 +0800

    First commit

--- Sort/Count.h
+++ Sort/Count.h
@@ -10,6 +10,6 @@ void Count(std::vector<int> &VecRaw, std::vector<int> &VecObj) {
     vecCount[VecRaw[i]]++;
   for (int i = 1; i < k; i++)
     vecCount[i] += vecCount[i - 1];
-  for (int i = VecRaw.size(); i > 0; i--) // 此处逆序是为了保持相同键值的稳定性
+  for (int i = VecRaw.size(); i > 0; i--)
     VecObj[--vecCount[VecRaw[i - 1]]] = VecRaw[i - 1];
 }
