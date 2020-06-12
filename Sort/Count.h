0bd9446c2d49d78132db900eaecc178b7a885ba9
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-06-12 14:42:23 +0800

    First commit

--- Sort/Count.h
+++ Sort/Count.h
@@ -3,7 +3,7 @@
 #include <vector>
 using namespace std;
 void Count(vector<int> &VecRaw, vector<int> &VecObj) {
-  if (VecRaw.empty)
+  if ((VecRaw.empty()))
     return;
   int k = *max_element(VecRaw.begin(), VecRaw.end()) + 1;
   vector<int> vecCount(k, 0);
@@ -14,17 +14,18 @@ void Count(vector<int> &VecRaw, vector<int> &VecObj) {
   for (int i = VecRaw.size(); i > 0; i--)
     VecObj[--vecCount[VecRaw[i - 1]]] = VecRaw[i - 1];
 }
+// another method
 vector<int> CountSort(vector<int> &nums) {
   if (nums.empty())
     return {};
   int low = *min_element(nums.begin(), nums.end());
   int high = *max_element(nums.begin(), nums.end());
-  int n = high - low + 1;
-  vector<int> buckets(n);
+  int k = high - low + 1;
+  vector<int> buckets(k);
   for (auto x : nums)
     ++buckets[x - low];
   vector<int> res;
-  for (int i = 0; i < n; ++i) {
+  for (int i = 0; i < k; ++i) {
     for (int j = 0; j < buckets[i]; ++j) {
       res.push_back(i + low);
     }
