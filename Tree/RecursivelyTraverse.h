d07950850b12548834b18488391d9e0ebece5e6b
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-06-14 22:24:07 +0800

    First commit

--- Tree/RecursivelyTraverse.h
+++ Tree/RecursivelyTraverse.h
@@ -1,23 +1,25 @@
-##include<iostream> struct Tree {
+#include <iostream>
+using namespace std;
+struct Tree {
   int Element;
   Tree *Left;
   Tree *Right;
 };
-void PreOrder(Tree T) {
+void PreOrder(Tree *T) {
   if (T != nullptr) {
     std::cout << T->Element << endl;
     PreOrder(T->Left);
     PreOrder(T->Right);
   }
 }
-void InOrder(Tree T) {
+void InOrder(Tree *T) {
   if (T != nullptr) {
     InOrder(T->Left);
     std::cout << T->Element << endl;
     InOrder(T->Right);
   }
 }
-void PostOrder(Tree T) {
+void PostOrder(Tree *T) {
   if (T != nullptr) {
     PostOrder(T->Left);
     PostOrder(T->Right);
