eb1a166a1060eafdd79f877b94864e8e365dff57
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-06-17 23:09:21 +0800

    First commit

---
+++ Graph/Dijkstra.h
@@ -0,0 +1,10 @@
+#include <vector>
+using namespace std;
+static const int maxv = 10;
+static vector<int> G[maxv];
+static vector<int> visited(maxv, 0);
+static int v, e;
+void Dijkstra(vector<int> G) {
+  for (int i = 0; i < v; i++) {
+  }
+}
