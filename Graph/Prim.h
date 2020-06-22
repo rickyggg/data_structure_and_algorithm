b9b979972b5ba4521810847dfa43b9df78b2fdb8
guojiarui.Windows 10 <guojiarui@smail.nju.edu.cn>
2020-06-22 21:17:52 +0800

    First Commit

--- Graph/Prim.h
+++ Graph/Prim.h
@@ -17,3 +17,23 @@ int findMin() {
   }
   return k;
 }
+int prim() {
+  for (int i = 1; i <= v; i++) {
+    cost[i] = G[1][i];
+  }
+  cost[1] = 0;
+  for (int i = 1; i < v; i++) {
+    int k = findMin();
+    if (k != 0) {
+      ret_cost += cost[k];
+      cost[k] = 0;
+      for (int j = 2; j <= v; j++) {
+        if (cost[j] && G[k][j] < cost[j])
+          cost[j] = G[k][j];
+      }
+    } else {
+      return -1;
+    }
+  }
+  return ret_cost;
+}
