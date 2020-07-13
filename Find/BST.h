b4221f0c25232f617ab09d84eec8a8301689290e
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-07-13 12:56:06 +0800

    fix errors

--- Find/BST.h
+++ Find/BST.h
@@ -16,3 +16,45 @@ TreeNode *SearchBST(int x, TreeNode *T) {
   else
     return T;
 }
+TreeNode *FindMin(TreeNode *T) {
+  if (T == nullptr)
+    return nullptr;
+  else if (T->Left == nullptr)
+    return T;
+  else
+    return FindMin(T->Left);
+}
+TreeNode *Insert(int x, TreeNode *T) {
+  if (T == nullptr) {
+    T = new TreeNode;
+    T->Element = x;
+    T->Left->Right = nullptr;
+  } else if (x < T->Element) {
+    T->Left = Insert(x, T->Left);
+  } else if (x > T->Element) {
+    T->Right = Insert(x, T->Right);
+  }
+  return T;
+}
+TreeNode *Delete(int x, TreeNode *T) {
+  TreeNode *Tmp;
+  if (T == nullptr) {
+    // error;
+  } else if (x < T->Element) {
+    T->Left = Delete(x, T->Left);
+  } else if (x > T->Element) {
+    T->Right = Delete(x, T->Right);
+  } else if (T->Left && T->Right) {
+    Tmp = FindMin(T->Right);
+    T->Element = Tmp->Element;
+    T->Right = Delete(T->Element, T->Right);
+  } else { // one & zero children
+    Tmp = T;
+    if (T->Left == nullptr)
+      T = T->Right;
+    else
+      T = T->Left;
+    delete Tmp;
+  }
+  return T;
+}
