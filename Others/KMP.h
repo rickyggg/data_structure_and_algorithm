66f5d73dc06a9b0de9a62098363f79c82a3d7c56
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-08-06 21:50:48 +0800

    add KMP

--- Others/KMP.h
+++ Others/KMP.h
@@ -1,3 +1,18 @@
 #include <string>
 using namespace std;
-int KMP(string t, string p);
+int Next[10];
+int KMP(string t, string p) {
+  int i, j;
+  while (i < t.size() && j < p.size()) {
+    if (j == -1 || t[i] == p[j]) {
+      i++;
+      j++;
+    } else {
+      j = Next[j];
+    }
+  }
+  if (j == p.size())
+    return i - j;
+  else
+    return -1;
+};
