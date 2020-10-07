6a87dbeb51811e94709f88f19f4f9ed8159d1c03
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-10-07 19:02:35 +0800

    add no recursive solution

--- Graph/DFS.cpp
+++ Graph/DFS.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stack>
 #include <vector>
 using namespace std;
 static const int maxv = 10;
@@ -12,16 +13,33 @@ void AddEdge(vector<int> adjacency[], int u, int v) {
 }
 void DFS_AdjMat(int start) {
   visited[start] = 1;
-  printf("%d ", start);
+  cout << start << " ";
   for (int i = 0; i < v; ++i) {
     if (!visited[i] && G_AdjMat[start][i])
       DFS_AdjMat(i);
   }
   return;
 }
+void DFS_AdjMat_WithoutRecursive(int start) {
+  stack<int> temp;
+  temp.push(start);
+  while (!temp.empty()) {
+    start = temp.top();
+    temp.pop();
+    if (!visited[start]) {
+      cout << start << " ";
+      visited[start] = 1;
+    }
+    for (int i = 0; i < v; ++i) {
+      if (!visited[i] && G_AdjMat[start][i])
+        temp.push(i);
+    }
+  }
+  return;
+}
 void DFS_AdjTab(int start) {
   visited[start] = 1;
-  printf("%d ", start);
+  cout << start << " ";
   int size = G_AdjTab[start].size();
   for (int i = 0; i < size; ++i) {
     if (!visited[G_AdjTab[start][i]] && G_AdjTab[start][i])
