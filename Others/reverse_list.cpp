e944ae9d2139feead20ab171daea8e14114fa527
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-10-11 20:03:33 +0800

    add the situation

--- Others/reverse_list.cpp
+++ Others/reverse_list.cpp
@@ -10,3 +10,20 @@ ListNode *ReverseList(ListNode *head) {
   head->next = nullptr;
   return new_head;
 }
+ListNode *successor = nullptr;
+ListNode *ReverseK(ListNode *head, int k) {
+  if (k == 1) {
+    successor = head->next;
+    return head;
+  }
+  ListNode *new_head = ReverseK(head->next, k - 1);
+  head->next->next = head;
+  head->next = successor;
+  return new_head;
+}
+ListNode *ReverseBetween(ListNode *head, int l, int r) {
+  if (l == 1)
+    return ReverseK(head, r);
+  head->next = ReverseBetween(head->next, l - 1, r - 1);
+  return head;
+}
