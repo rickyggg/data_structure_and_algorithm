e65ed64b4f8b585a4294225bfb17b8e292efc11f
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-12-12 18:31:12 +0800

    add randcoin

--- Others/randcoin.cpp
+++ Others/randcoin.cpp
@@ -5,19 +5,19 @@ using namespace std;
 int coin() { return (rand() % 10) > 4; }
 
 int coin_new() {
-  while (true) {
-    int a = coin();
-    if (coin() != a)
-      return a;
+  while (1) {
+    int res = coin();
+    if (coin() != res)
+      return res;
   }
 }
 
 int main() {
-  int N = 1000000;
+  int n = 1000000;
   float count[2] = {0, 0};
-  for (int i = 0; i < N; i++)
+  for (int i = 0; i < n; i++)
     count[coin_new()]++;
 
-  cout << "0: " << count[0] / N << endl;
-  cout << "1: " << count[1] / N << endl;
+  cout << "0: " << count[0] / n << endl;
+  cout << "1: " << count[1] / n << endl;
 }
