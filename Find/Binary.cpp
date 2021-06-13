a39f5309e7d962121e78f3d461dafefc8e9dd091
guojiarui1102 <jiaruiguo1102@gmail.com>
2021-06-13 20:19:34 +0800

    feat: add binary search of right/left bound

--- Find/Binary.cpp
+++ Find/Binary.cpp
@@ -1,16 +1,51 @@
 // O(logN)
-int BinarySearch(int *list, int value, int len) {
-  int low = 0;
-  int high = len - 1;
-  while (low <= high) {
-    int mid = low + (high - low) / 2;
-    if (list[mid] > value) {
-      high = mid - 1;
-    } else if (list[mid] < value) {
-      low = mid + 1;
-    } else {
-      return list[mid];
+int BinarySearch(vector<int> &nums, int target) {
+  int left = 0, right = nums.size() - 1;
+  while (left <= right) {
+    int mid = left + (right - left) / 2;
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    } else if (nums[mid] == target) {
+      return mid;
     }
   }
   return -1;
+}
+
+int LeftBound(vector<int> &nums, int target) {
+  int left = 0, right = nums.size() - 1;
+  while (left <= right) {
+    int mid = left + (right - left) / 2;
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    } else if (nums[mid] == target) {
+      right = mid - 1;
+    }
+  }
+  if (left >= nums.size() || nums[left] != target) {
+    return -1;
+  }
+  return left;
+}
+
+int RightBound(vector<int> &nums, int target) {
+  int left = 0, right = nums.size() - 1;
+  while (left <= right) {
+    int mid = left + (right - left) / 2;
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    } else if (nums[mid] == target) {
+      left = mid + 1;
+    }
+  }
+  if (right >= nums.size() || nums[right] != target) {
+    return -1;
+  }
+  return right;
 }
